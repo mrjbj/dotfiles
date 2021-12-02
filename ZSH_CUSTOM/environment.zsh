fd21fea1324142f32cb5eaeca013bacb68af1631
Jason Jones <jason@brucejones.biz>
2021-12-02 14:36:22 -0500

    removed Github Access Token

--- ZSH_CUSTOM/environment.zsh
+++ ZSH_CUSTOM/environment.zsh
@@ -7,7 +7,6 @@ export HOMEBREW_REPOSITORY="/opt/homebrew";
 export HOMEBREW_SHELLENV_PREFIX="/opt/homebrew";
 export MANPATH="/opt/homebrew/share/man${MANPATH+:$MANPATH}:";
 export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}";
-export HOMEBREW_GITHUB_API_TOKEN="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";
 
 #----------
 # history
