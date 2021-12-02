a15d8d9f6336a40e0ca1902bda35233fced18d27
Jason Jones <jason@brucejones.biz>
2021-12-02 10:47:15 -0500

    updated path to include gnu grep

--- ZSH_CUSTOM/path.zsh
+++ ZSH_CUSTOM/path.zsh
@@ -5,4 +5,5 @@ export PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}";
 export PATH="$HOME/.emacs.d/bin:$PATH"
 
 # Core binaries to replace older Mac Versions 
+export PATH="$(brew --prefix grep)/libexec/gnubin:$PATH"
 export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
