e2911e011da5265fbc47bd6a7165ddf5ad65c2e9
Jason Jones <jason@brucejones.biz>
2023-09-27 12:14:48 -0400

    mapping changes

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,48 +7,81 @@ local config = {}
 -- options
 config = wezterm.config_builder()
 config.adjust_window_size_when_changing_font_size = false
-config.automatically_reload_config = false
+config.automatically_reload_config = true
 config.color_scheme = "Bamboo"
 config.hide_tab_bar_if_only_one_tab = true
 config.leader = { key = "Space", mods = "CTRL|SHIFT" }
 config.use_dead_keys = false
 config.warn_about_missing_glyphs = true
-
+config.command_palette_font_size = 20.0
+config.font_size = 20.0
+config.audible_bell = "Disabled"
+config.visual_bell = {
+	fade_in_duration_ms = 75,
+	fade_out_duration_ms = 75,
+	target = "CursorColor",
+}
 -- fonts
 config.font = wezterm.font({
-  family = "FiraCode Nerd Font",
-  weight = 450,
-  stretch = "Normal",
-  style = "Normal",
-  harfbuzz_features = { "liga=1", "cv14", "cv24", "ss03", "ss04", "ss05", "ss07" },
+	family = "FiraCode Nerd Font",
+	weight = 450,
+	stretch = "Normal",
+	style = "Normal",
+	harfbuzz_features = { "liga=1", "cv14", "cv24", "ss03", "ss04", "ss05", "ss07" },
 })
 
 --keys
 config.keys = {
-  -- split horizontally
-  {
-    key = "v",
-    mods = "ALT|CMD",
-    action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-  },
+	-- reset CTRL|SHIFT L so can use later below
+	{ key = "L",         mods = "CTRL|SHIFT", action = "DisableDefaultAssignment" },
+	-- split horizontally
+	{
+		key = "H",
+		mods = "CTRL|SHIFT|ALT",
+		action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
 
-  -- Split vertically
-  {
-    key = "s",
-    mods = "CTRL|SHIFT",
-    action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
-  },
+	-- Split vertically
+	{
+		key = "V",
+		mods = "CTRL|SHIFT|ALT",
+		action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
 
-  -- Scroll up 1 line
-  { key = "UpArrow",   mods = "SHIFT", action = act.ScrollByLine(-1) },
+	-- switch pane left
+	{
+		key = "H",
+		mods = "CTRL|SHIFT",
+		action = act.ActivatePaneDirection("Left"),
+	},
+	-- switch pane right
+	{
+		key = "L",
+		mods = "CTRL|SHIFT",
+		action = act.ActivatePaneDirection("Right"),
+	},
+	-- switch pane up
+	{
+		key = "K",
+		mods = "CTRL|SHIFT",
+		action = act.ActivatePaneDirection("Up"),
+	},
+	-- switch pane down
+	{
+		key = "J",
+		mods = "CTRL|SHIFT",
+		action = act.ActivatePaneDirection("Down"),
+	},
+	-- Scroll up 1 line
+	{ key = "UpArrow",   mods = "SHIFT",      action = act.ScrollByLine(-1) },
 
-  -- Scroll down 1 line
-  { key = "DownArrow", mods = "SHIFT", action = act.ScrollByLine(1) },
+	-- Scroll down 1 line
+	{ key = "DownArrow", mods = "SHIFT",      action = act.ScrollByLine(1) },
 
-  -- Scrool to bottom
-  { key = "End",       mods = "SHIFT", action = act.ScrollToBottom },
+	-- Scrool to bottom
+	{ key = "End",       mods = "SHIFT",      action = act.ScrollToBottom },
 
-  -- Scrool to top
-  { key = "Home",      mods = "SHIFT", action = act.ScrollToTop },
+	-- Scrool to top
+	{ key = "Home",      mods = "SHIFT",      action = act.ScrollToTop },
 }
 return config
--- ZSH_CUSTOM/nvim-config.zsh
+++ ZSH_CUSTOM/nvim-config.zsh
@@ -1,13 +1,14 @@
 # these aliases configure direct access w/o having to use the nvims() picker implemented below.
-alias nvim-lazy="NVIM_APPNAME=LazyVim nvim"
-alias nvim-kick="NVIM_APPNAME=kickstart nvim"
-alias nvim-chad="NVIM_APPNAME=NvChad nvim"
-alias nvim-astro="NVIM_APPNAME=nvim-astro nvim"
-alias nvim="NVIM_APPNAME=nvim-astro nvim"  ## default 
+alias nvim-lazy="NVIM_APPNAME=LazyVim \nvim"
+alias nvim-kick="NVIM_APPNAME=kickstart \nvim"
+alias nvim-chad="NVIM_APPNAME=NvChad \nvim"
+alias nvim-astro="NVIM_APPNAME=nvim-astro \nvim"
+alias nvim-lvim="NVIM_APPNAME=nvim-lvim \nvim"
+alias nvim="NVIM_APPNAME=nvim-astro \nvim"  ## default 
 
 # nvims picker
 function nvims() {
-  items=("Default" "LazyVim" "LunarVim" "Chad" "AstroVim")
+  items=("Default" "LazyVim" "LunarVim" "Chad" "AstroVim" "Lvim")
   config=$(printf "%s\n" "${items[@]}" | fzf --prompt=" Neovim Config -> " --height=~50% --layout=reverse --border --exit-0)
   if [[ -z $config ]]; then
     echo "Nothing selected"
@@ -18,7 +19,7 @@ function nvims() {
     config="nvim-${(L)config%Vim}"
   fi
   printf $config
-  NVIM_APPNAME=$config nvim $@
+  NVIM_APPNAME=$config \nvim $@
 }
 
 bindkey -s ^a "nvims\n" 
