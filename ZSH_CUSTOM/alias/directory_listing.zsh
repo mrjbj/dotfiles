4ca96232571b1e6e81b573dafea0527fb6735545
Jason Jones <jason@brucejones.biz>
2024-01-30 19:54:24 -0500

    updated

---
+++ ZSH_CUSTOM/alias/directory_listing.zsh
@@ -0,0 +1,11 @@
+# list aliases
+alias ll="/opt/homebrew/opt/coreutils/libexec/gnubin/ls -AhlFo --color=auto --group-directories-first --hyperlink=auto"
+alias ldot='ls -ld .* --color=auto --hyperlink=auto'
+alias lsd="ls -ld .*"
+alias l="ls -lFh --color=auto --hyperlink=auto"
+alias la="ls -lAFh --color=auto --hyperlink=auto"
+alias lr="ls -tRFh --color=auto --hyperlink=auto"
+alias lS="ls -1FSsh --color=auto --hyperlink=auto"
+alias lart="ls -1Fcart --color=auto --hyperlink=auto"
+alias lrt="ls -1Fcrt --color=auto --hyperlink=auto"
+alias r="ranger"
