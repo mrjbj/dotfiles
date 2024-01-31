32e3056f90a4fcabf0d6b40164e4324551b5d338
Jason Jones <jason@brucejones.biz>
2024-01-31 11:52:31 -0500

    removed secret

--- ZSH_CUSTOM/programs/brew.zsh
+++ ZSH_CUSTOM/programs/brew.zsh
@@ -9,10 +9,6 @@ export HOMEBREW_REPOSITORY="/opt/homebrew";
 export HOMEBREW_SHELLENV_PREFIX="/opt/homebrew";
 export MANPATH="/opt/homebrew/share/man${MANPATH+:$MANPATH}:";
 export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}";
- 
-# cannot upload this to github. 
-# set gitignore to ignore it
-export HOMEBREW_GITHUB_API_TOKEN="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";
 
 # override older MacOS system utils
 export PATH="$(brew --prefix grep)/libexec/gnubin:$PATH"
